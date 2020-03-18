2eb4dcc8a5873847950c1b78abf5a2844cecd2f2
ahcdw <xing.wang@qiaofang.com>
2020-03-18 09:26:04 +0800

    1.二维数组中的查找

---
+++ nowcoder/src/com/wx/swordMeansOffer/Solution1.java
@@ -0,0 +1,22 @@
+package com.wx.swordMeansOffer;
+/**
+ * 在一个二维数组中（每个一维数组的长度相同），每一行都按照从左到右递增的顺序排序，
+ * 每一列都按照从上到下递增的顺序排序。请完成一个函数，输入这样的一个二维数组和一个整数，
+ * 判断数组中是否含有该整数。
+ */
+public class Solution1 {
+    public static boolean Find(int target, int[][] array) {
+        if (array.length == 0 || array[0].length == 0) return false;
+        for (int i = 0; i < array.length && target >= array[i][0]; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                if (array[i][j] == target) return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[][] array = {{1, 2, 8, 9}, {2, 4, 9, 12}, {4, 7, 10, 13}, {6, 8, 11, 15}};
+        System.out.println(Find(7, array));
+    }
+}
