5abda0a0c32ef8906a89f3bb272034d14f4f9222
ahcdw <xing.wang@qiaofang.com>
2020-03-18 14:18:22 +0800

    5.用两个栈实现队列

---
+++ nowcoder/src/com/wx/swordMeansOffer/Solution5.java
@@ -0,0 +1,36 @@
+package com.wx.swordMeansOffer;
+
+import java.util.Stack;
+
+/**
+ * 用两个栈来实现一个队列，完成队列的Push和Pop操作。 队列中的元素为int类型。
+ */
+public class Solution5 {
+    Stack<Integer> stack1 = new Stack<Integer>();
+    Stack<Integer> stack2 = new Stack<Integer>();
+
+    public void push(int node) {
+        stack1.add(node);
+
+    }
+
+    public int pop() {
+        if (stack2.isEmpty()) {
+            while (!stack1.isEmpty()) {
+                stack2.add(stack1.pop());
+            }
+        }
+        return stack2.pop();
+    }
+
+    public static void main(String[] args) {
+        Solution5 s=new Solution5();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        System.out.println(s.pop());
+        System.out.println(s.pop());
+        System.out.println(s.pop());
+
+    }
+}
