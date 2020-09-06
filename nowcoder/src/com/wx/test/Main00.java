940095362721e5bdf0ff1f0a2bb32f71f9ea60fd
ahcdw <xing.wang@qiaofang.com>
2020-09-06 23:46:39 +0800

    测试新建分支

--- nowcoder/src/com/wx/test/Main00.java
+++ nowcoder/src/com/wx/test/Main00.java
@@ -8,41 +8,7 @@ public class Main00 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int T = sc.nextInt();        //测试组数样例
-        boolean[] res = new boolean[T];
-        for (int i = 0; i < T; i++) {
-            res[i]=true;
-        }
-        for (int i = 0; i < T; i++) {
-            int[] arr = new int[12];
-
-            for (int j = 0; j < 12; j++) {
-                arr[j]=sc.nextInt();
-            }
-            Map<Integer,Integer> map = new HashMap<>();
-            for (int j = 0; j < arr.length; j++) {
-                if (map.containsKey(arr[j])){
-                    int count = map.get(arr[j])+1;
-                    map.put(arr[j],count);
-                }else {
-                    map.put(arr[j],1);
-                }
-            }
-
-            for (int x:map.keySet()){
-                if (map.get(x)!=4){
-                    res[i]=false;
-                    break;
-                }
-            }
-        }
-        for (boolean b:res){
-            if (b){
-                System.out.println("POSSIBLE");
-            }else {
-                System.out.println("IMPOSSIBLE");
-            }
-        }
-
+        System.out.println("1111");
     }
 
 
