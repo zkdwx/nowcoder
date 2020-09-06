b4cd72982dcaba02b3b04a2c067e2c71d639bdba
ahcdw <xing.wang@qiaofang.com>
2020-09-07 00:25:15 +0800

    only once

--- nowcoder/src/com/wx/test/Main200.java
+++ nowcoder/src/com/wx/test/Main200.java
@@ -1,14 +1,15 @@
 package com.wx.test;
 
 
-import java.util.Arrays;
-import java.util.Map;
-import java.util.Scanner;
-import java.util.TreeMap;
 
 public class Main200 {
     public static void main(String[] args) {
         System.out.println("666666666666666666");
+        System.out.println("666666666666666666");
+        System.out.println("666666666666666666");
+        System.out.println("666666666666666666");
+
+
     }
 
 
