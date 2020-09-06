04fe2b3d165c23788480df3c50410958f1841640
ahcdw <xing.wang@qiaofang.com>
2020-09-06 23:58:52 +0800

    6666

--- nowcoder/src/com/wx/test/Main200.java
+++ nowcoder/src/com/wx/test/Main200.java
@@ -8,36 +8,7 @@ import java.util.TreeMap;
 
 public class Main200 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();       //乘客数量
-        if (n < 1 || n > 1000) return;
-        Map<Integer, Integer> map = new TreeMap<>();
-        int[] arr = new int[n];
-        int[] res = new int[n];  //结果标志
-        for (int i = 0; i < n; i++) {
-            int key = sc.nextInt();
-            int value = sc.nextInt();
-            if (key < 1 || key > 100000 || value < 1 || value > 100000) return;
-            map.put(key, value);
-            arr[i] = key;
-        }
-
-        Arrays.sort(arr);
-        res[0] = 1;
-        for (int i = 1; i <= n - 1; i++) {
-            int last = map.get(arr[i - 1]);
-            int curr = map.get(arr[i]);
-            if (curr >= last) {
-                res[i] = 1;
-            } else {
-                res[i] = 0;
-            }
-        }
-        int sum = 0;
-        for (int x : res) {
-            sum += x;
-        }
-        System.out.println(sum);
+        System.out.println("666666666666666666");
     }
 
 
